d9d49c7e363ef91a7dd6433d5f51698a4b32a8ed
Antonin Raffin <antonin.raffin@ensta.org>
2021-12-14 12:24:29 +0100

    Add autoencoder wrapper

---
+++ ae/wrapper.py
@@ -0,0 +1,20 @@
+import os
+
+import gym
+
+from ae.autoencoder import load_ae
+
+
+class AutoencoderWrapper(gym.Wrapper):
+    def __init__(self, env, ae_path: Optional[str] = os.environ.get("AAE_PATH")):
+        super().__init__(env)
+        assert ae_path is not None
+        self.ae = load_ae(ae_path)
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(self.ae.z_size), dtype=np.float32)
+
+    def reset(self):
+        return self.ae.encode_from_raw_image(self.env.reset())
+
+    def step(self, action):
+        obs, reward, done, infos = self.env.step(action)
+        return self.ae.encode_from_raw_image(obs), reward, done, infos
