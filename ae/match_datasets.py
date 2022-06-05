33d4f8b2f12ad0a1be1d2475c10e4a599715e26b
Antonin Raffin <antonin.raffin@ensta.org>
2022-06-05 15:04:31 +0200

    Add training code for matching datasets

--- ae/match_datasets.py
+++ ae/match_datasets.py
@@ -1,83 +1,87 @@
 import argparse
 import pickle
 from pathlib import Path
+from typing import List, Tuple
 
 import cv2  # pytype: disable=import-error
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 
-from ae.autoencoder import load_ae
+from ae.autoencoder import Autoencoder, load_ae
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--folder", help="Log folders", type=str, nargs="+", required=True)
-parser.add_argument("-ae", "--ae-path", help="Path to saved AE", type=str)
-parser.add_argument("-n", "--n-samples", help="Max number of samples", type=int, default=20)
-parser.add_argument("--seed", help="Random generator seed", type=int, default=0)
-args = parser.parse_args()
 
-np.random.seed(args.seed)
+def prepare_datasets(
+    autoencoder: Autoencoder, folders: List[str], normalize: bool = False
+) -> Tuple[List[np.ndarray], List[List[str]]]:
+    datasets = []
+    names = []
+    for folder in folders:
+        with open(Path(folder) / "infos.pkl", "rb") as f:
+            infos = pickle.load(f)
 
-autoencoder = None
-if args.ae_path is not None:
-    autoencoder = load_ae(args.ae_path)
-
-datasets = []
-info_dicts = []
-names = []
-for folder in args.folder:
-    with open(Path(folder) / "infos.pkl", "rb") as f:
-        infos = pickle.load(f)
-
-    # Preprocess and create features
-    if autoencoder is not None:
+        # Preprocess and create features
         dataset = np.zeros((len(infos), autoencoder.z_size + 1))
         for i, (name, info) in enumerate(infos.items()):
             input_image = cv2.imread(str(Path(folder) / f"{name}.jpg"))
             encoded_image = autoencoder.encode_from_raw_image(input_image).flatten()
             dataset[i][0] = info["cte"]
             dataset[i][1:] = encoded_image
-    else:
-        dataset = [[frame["cte"], 0.0] for frame in infos.values()]
 
-    datasets.append(np.array(dataset))
-    info_dicts.append(infos)
-    names.append(list(infos.keys()))
+        datasets.append(np.array(dataset))
+        names.append(list(infos.keys()))
 
-# Normalize according to first dataset
-normalizer = StandardScaler().fit(datasets[0])
-for i, dataset in enumerate(datasets):
-    datasets[i] = normalizer.transform(dataset)
-    # More weight for CTE
-    datasets[i][:, 0] *= 5
+    # Normalize according to first dataset
+    normalizer = StandardScaler().fit(datasets[0])
+    for i, dataset in enumerate(datasets):
+        if normalize:
+            datasets[i] = normalizer.transform(dataset)
+        # More weight for CTE
+        datasets[i][:, 0] *= 10
+    return datasets, names
 
-# Create KNN with first dataset
-knn = NearestNeighbors(n_neighbors=2, algorithm="ball_tree").fit(datasets[0])
 
-other_dataset = datasets[1]
-random_samples = np.random.permutation(len(other_dataset))[: args.n_samples]
-image_grid = []
-for idx, sample in enumerate(random_samples):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", help="Log folders", type=str, nargs="+", required=True)
+    parser.add_argument("-ae", "--ae-path", help="Path to saved AE", type=str, required=True)
+    parser.add_argument("-n", "--n-samples", help="Max number of samples", type=int, default=20)
+    parser.add_argument("--seed", help="Random generator seed", type=int, default=0)
+    args = parser.parse_args()
 
-    _, neighbor_indices = knn.kneighbors([other_dataset[sample]])
-    neighbor_indices = neighbor_indices.flatten()
+    np.random.seed(args.seed)
 
-    image1_path = str(Path(args.folder[1]) / f"{names[1][sample]}.jpg")
-    image1 = cv2.imread(image1_path)
+    autoencoder = load_ae(args.ae_path)
 
-    neighbors = []
-    for neighbor_idx in neighbor_indices:
-        image_path = str(Path(args.folder[0]) / f"{names[0][neighbor_idx]}.jpg")
-        neighbor_image = cv2.imread(image_path)
-        neighbors.append(neighbor_image)
-    image_grid.append(np.hstack([image1] + neighbors))
+    datasets, names = prepare_datasets(autoencoder, args.folder)
 
-    if (idx + 1) % 5 == 0:
-        grid = np.array(image_grid)
-        cv2.imshow("Image grid", grid.reshape((-1,) + grid.shape[2:]))
-        # stop if escape is pressed
-        k = cv2.waitKey(0) & 0xFF
+    # Create KNN with first dataset
+    knn = NearestNeighbors(n_neighbors=2, algorithm="ball_tree").fit(datasets[0])
 
-        if k == 27:
-            break
-        image_grid = []
+    other_dataset = datasets[1]
+    random_samples = np.random.permutation(len(other_dataset))[: args.n_samples]
+    image_grid = []
+    for idx, sample in enumerate(random_samples):
+
+        _, neighbor_indices = knn.kneighbors([other_dataset[sample]])
+        neighbor_indices = neighbor_indices.flatten()
+
+        image1_path = str(Path(args.folder[1]) / f"{names[1][sample]}.jpg")
+        image1 = cv2.imread(image1_path)
+
+        neighbors = []
+        for neighbor_idx in neighbor_indices:
+            image_path = str(Path(args.folder[0]) / f"{names[0][neighbor_idx]}.jpg")
+            neighbor_image = cv2.imread(image_path)
+            neighbors.append(neighbor_image)
+        image_grid.append(np.hstack([image1] + neighbors))
+
+        if (idx + 1) % 5 == 0:
+            grid = np.array(image_grid)
+            cv2.imshow("Image grid", grid.reshape((-1,) + grid.shape[2:]))
+            # stop if escape is pressed
+            k = cv2.waitKey(0) & 0xFF
+
+            if k == 27:
+                break
+            image_grid = []
---
+++ ae/train_match.py
@@ -0,0 +1,130 @@
+"""
+Train an autoencoder model using saved images in a folder
+"""
+import argparse
+import os
+import random
+import time
+
+import cv2  # pytype: disable=import-error
+import numpy as np
+import torch as th
+from sklearn.neighbors import NearestNeighbors
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, Dataset
+
+# from tqdm.rich import tqdm
+from tqdm import tqdm
+
+from ae.autoencoder import Autoencoder, preprocess_image, preprocess_input
+from ae.match_datasets import prepare_datasets
+
+
+class RacingDataset(Dataset):
+    def __init__(self, teacher: Autoencoder, teacher_folder: str, student_folder: str):
+        datasets, names = prepare_datasets(teacher, [teacher_folder, student_folder], normalize=False)
+        self.teacher = teacher
+        self.teacher_folder = teacher_folder
+        self.student_folder = student_folder
+        self.teacher_dataset = datasets[0]
+        self.student_dataset = datasets[1]
+        self.names = names[1]
+
+        # Create KNN with first dataset
+        self.knn = NearestNeighbors(n_neighbors=1, algorithm="ball_tree").fit(self.teacher_dataset)
+
+    def __len__(self):
+        return len(self.names)
+
+    def __getitem__(self, idx):
+        if th.is_tensor(idx):
+            idx = idx.tolist()
+
+        _, neighbor_indices = self.knn.kneighbors([self.student_dataset[idx]])
+        neighbor_indices = neighbor_indices.flatten()
+        # Remove CTE from target
+        target = self.teacher_dataset[neighbor_indices[0]][1:]
+
+        img_name = os.path.join(self.student_folder, f"{self.names[idx]}.jpg")
+        image = preprocess_image(cv2.imread(img_name), convert_to_rgb=False, normalize=False)
+        # Normalize
+        image = preprocess_input(image.astype(np.float32), mode="rl")
+        return {"image": th.as_tensor(image), "target": th.as_tensor(target).float()}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ae", "--ae-path", help="Path to saved autoencoder", type=str, required=True)
+    parser.add_argument(
+        "-s", "--student-folder", help="Path to folder containing images for training", type=str, required=True
+    )
+    parser.add_argument(
+        "-t", "--teacher-folder", help="Path to folder containing images for matching", type=str, required=True
+    )
+    parser.add_argument("--seed", help="Random generator seed", type=int)
+    parser.add_argument("-bs", "--batch-size", help="Batch size", type=int, default=64)
+    parser.add_argument("--learning-rate", help="Learning rate", type=float, default=1e-4)
+    parser.add_argument("--n-epochs", help="Number of epochs", type=int, default=10)
+    parser.add_argument("--verbose", help="Verbosity", type=int, default=1)
+    parser.add_argument("--num-threads", help="Number of threads for PyTorch (-1 to use default)", default=-1, type=int)
+    args = parser.parse_args()
+
+    if args.num_threads > 0:
+        th.set_num_threads(args.num_threads)
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        th.manual_seed(args.seed)
+        if th.cuda.is_available():
+            th.backends.cudnn.deterministic = True
+            th.backends.cudnn.benchmark = False
+
+    teacher = Autoencoder.load(args.ae_path)
+    student = Autoencoder.load(args.ae_path)
+    args.z_size = teacher.z_size
+    teacher.to(teacher.device)
+    student.to(student.device)
+
+    best_loss = np.inf
+    ae_id = int(time.time())
+    save_path = f"logs/match_ae-{args.z_size}_{ae_id}.pkl"
+    best_model_path = f"logs/match_ae-{args.z_size}_{ae_id}_best.pkl"
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    dataset = RacingDataset(teacher, args.teacher_folder, args.student_folder)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    try:
+        for epoch in range(args.n_epochs):
+            pbar = tqdm(total=len(dataloader))
+            train_loss = 0
+            for sample in dataloader:
+                obs = th.as_tensor(sample["image"]).to(student.device)
+                target = th.as_tensor(sample["target"]).to(student.device)
+
+                student.optimizer.zero_grad()
+
+                predicted_latent = student.encode_forward(obs)
+                loss = F.mse_loss(predicted_latent, target)
+
+                loss.backward()
+                train_loss += loss.item()
+                student.optimizer.step()
+
+                pbar.update(1)
+            pbar.close()
+            print(f"Epoch {epoch + 1:3}/{args.n_epochs}")
+            print("Loss:", train_loss)
+
+            # TODO: use validation set
+            if train_loss < best_loss:
+                best_loss = train_loss
+                print(f"Saving best model to {best_model_path}")
+                student.save(best_model_path)
+
+    except KeyboardInterrupt:
+        pass
+
+    print(f"Saving to {save_path}")
+    student.save(save_path)
